05bc99f19f6587afcbdb8f74fe214bd66190d8fc
ryuyasamaaaa <ryuyasamaaaa@gmail.com>
2019-03-14 22:49:17 +0900

    fix

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,36 @@ var app = require('./../app');
 
 /*--- /api/usersにGETアクションでアクセスしたときの処理 ---*/
 
+/*--- /api/users/loginにPOSTアクションでアクセスしたときの処理 ---*/
+router.post('/login', (req, res) => {
+    var sql = 'select * from users where email = ? and password = ?';
+    app.connection.query(sql, [req.body.email, req.body.password], (error, results, fields) => {
+
+        if (results == '') {
+            console.log('データが存在しません');
+            
+            var param = {'message': 'POSTアクションのリクエストに失敗しました'};
+            res.header('Content-Type', 'application/json; charset=utf-8')
+                .status(401)
+                .send(param);
+            console.log(req.body);
+
+        } else {
+            console.log('ログイン可能');
+            console.log(results);
+
+            var param = {'message': 'POSTアクションのリクエストに成功しました'};
+            res.header('Content-Type', 'application/json; charset=utf-8')
+                .status(200)
+                .send(param);
+            console.log(req.body);
+        }
+    });
+});
 
 /*--- /api/users/signupにPOSTアクションでアクセスしたときの処理 ---*/
 router.post('/signup', (req, res) => {
-    var sql = 'select * users where uid = ?';
+    var sql = 'select * from users where uid = ?';
     app.connection.query(sql, req.body.uid, (error, results, fields) => {
 
         if (results == '') {
